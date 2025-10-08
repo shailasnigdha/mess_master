aa19370b5767f5614862822b3e6b725988f3bb4d
510nix <anikanawer10@gmail.com>
2025-10-08 15:03:42 +0600

    fixed some design issues

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -32,7 +32,6 @@ $adminName = $_SESSION['name'] ?? $_SESSION['username'];
                 <li><a href="admin_manage_users.php">Manage Users</a></li>
                 <li><a href="admin_notices.php">Notices</a></li>
                 <li><a href="admin_meals.php">Meals</a></li>
-                <li><a href="admin_profile.php">Profile</a></li>
                 <li><a href="logout.php" class="logout-link">Logout</a></li>
             </ul>
         </div>
